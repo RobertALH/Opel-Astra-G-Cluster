2960593a98263eadfb3d4addf2a4a246e5333c3d
RobertALH <robert19alh@gmail.com>
2026-05-27 00:52:06 +0300

    Bug Fix Turometru
    Modificare prescaler turometru in functie de turatia setata pentru calibrare si miscare fluenta

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <delay.h>
@@ -8,12 +12,19 @@
 #define PIN_RELAY  PC0
 #define PIN_LIGHTS PC1
 
+volatile uint8_t tacho_extend_limit = 1;
+
 ISR(TIMER1_COMPA_vect) {
     GPIO_Toggle(GPIO_PORTB, PIN_SPEEDO);
 }
 
 ISR(TIMER2_COMPA_vect) {
-    GPIO_Toggle(GPIO_PORTD, PIN_TACHO);
+    static uint8_t extend = 0;
+    extend++;
+    if (extend >= tacho_extend_limit) {
+        GPIO_Toggle(GPIO_PORTD, PIN_TACHO);
+        extend = 0;
+    }
 }
 
 void Speedo_Init(void) {
@@ -36,16 +47,42 @@ void Set_Speed(uint16_t kmh) {
 
 void Tacho_Init(void) {
     GPIO_Init(GPIO_PORTD, PIN_TACHO, GPIO_OUTPUT);
-    TCCR2A = (1 << WGM21);
+    TCCR2A = (1 << WGM21); 
     TCCR2B = 0;
     TIMSK2 &= ~(1 << OCIE2A); 
 }
 
 void Set_RPM(uint16_t rpm) {
-    uint8_t top = (234375UL / rpm) - 1;
-    OCR2A = top; 
+    if (rpm > 1000) {
+        uint32_t corectie = ((uint32_t)rpm * rpm) / 144000UL;
+        if (rpm > corectie) {
+            rpm = rpm - corectie;
+        }
+    }
+
+    if (rpm < 400) {
+        TCCR2B = 0;               
+        TIMSK2 &= ~(1 << OCIE2A); 
+        GPIO_Write(GPIO_PORTD, PIN_TACHO, GPIO_LOW);
+        return;
+    }
     
-    TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20); 
+    uint8_t temp_ext;
+    uint32_t magic_precalc; 
+
+    if (rpm >= 4900) { temp_ext = 3;  magic_precalc = 1250000UL; } 
+    else if (rpm >= 3700) { temp_ext = 4;  magic_precalc = 937500UL; } 
+    else if (rpm >= 2500) { temp_ext = 6;  magic_precalc = 625000UL; } 
+    else if (rpm >= 1850) { temp_ext = 8;  magic_precalc = 468750UL; } 
+    else if (rpm >= 1250) { temp_ext = 12; magic_precalc = 312500UL; } 
+    else if (rpm >= 950)  { temp_ext = 16; magic_precalc = 234375UL; } 
+    else if (rpm >= 600)  { temp_ext = 24; magic_precalc = 156250UL; } 
+    else { temp_ext = 32; magic_precalc = 117187UL; }
+
+    uint8_t top = (magic_precalc / rpm) - 1;
+    tacho_extend_limit = temp_ext;
+    OCR2A = top; 
+    TCCR2B = (1 << CS22); 
     TIMSK2 |= (1 << OCIE2A); 
 }
 
@@ -61,19 +98,24 @@ int main(void) {
 
     sei();
 
-    Delay(1000);
+    Delay(1000); 
     GPIO_Write(GPIO_PORTC, PIN_RELAY, GPIO_LOW);
-    Delay(1000);
-    GPIO_Write(GPIO_PORTC, PIN_LIGHTS, GPIO_LOW);
-    Delay(1000);
+    Delay(1000);  
+    GPIO_Write(GPIO_PORTC, PIN_LIGHTS, GPIO_LOW); 
+    Delay(1000); 
 
     Set_RPM(3000);
     Set_Speed(100);
+    Delay(5000); 
+
+    Set_RPM(6000);
+    Set_Speed(200);
     Delay(5000);
 
-    Set_RPM(6000); 
-    Set_Speed(200);
+    Set_RPM(800); 
+    Set_Speed(20); 
 
     while (1) {
+   
     }
 }
